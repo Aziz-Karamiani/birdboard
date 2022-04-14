22d4d05824118ef02de19ace7a0772e577d8f7a6
a.karamiani <aziz.karamiani@gmail.com>
2022-04-14 15:24:24 +0430

    Install Framework

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_that_false_is_false()
+    {
+        $this->assertFalse(false);
+    }
+}
