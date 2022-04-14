9b05aa7187cc1651efbe560233e30e2202bd9248
a.karamiani <aziz.karamiani@gmail.com>
2022-04-15 02:02:04 +0430

    Validation added for create Project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
 
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
         $projects = Project::all();
@@ -16,9 +20,21 @@ class ProjectsController extends Controller
 
     /**
      * Store Project
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function store(): void
+    public function store(Request $request): \Illuminate\Routing\Redirector|\Illuminate\Contracts\Foundation\Application|RedirectResponse
     {
-        Project::create(request(["title", "description"]));
+        // Validation
+        $request->validate(["title" => "required", "description" => "required"]);
+
+        // get request data
+        $attributes = request(["title", "description"]);
+
+        // Store project
+        Project::create($attributes);
+
+        // Redirect to /projects
+        return redirect('/projects');
     }
 }
--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -15,7 +15,7 @@ class ProjectsTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_example(): void
     {
         $this->withExceptionHandling();
 
@@ -26,11 +26,31 @@ class ProjectsTest extends TestCase
         ];
 
         // Assert Database
-        $this->post('/projects', $attributes);
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
         $this->assertDatabaseHas("projects", $attributes);
 
         // Assert View
         $this->get('/projects')->assertStatus(200);
         $this->get('/projects')->assertSee($attributes["title"]);
+    }
+
+    /**
+     * Title validation must required.
+     */
+    public function test_title_validation(): void
+    {
+        $this->withExceptionHandling();
+
+        $this->post('/projects', [])->assertSessionHasErrors(['title']);
+    }
+
+    /**
+     * Description validation must required.
+     */
+    public function test_description_validation(): void
+    {
+        $this->withExceptionHandling();
+
+        $this->post('/projects', [])->assertSessionHasErrors(['description']);
     }
 }
